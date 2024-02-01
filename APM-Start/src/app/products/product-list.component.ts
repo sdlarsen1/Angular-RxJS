1f0f0428eb16834dcd8f6a9e76e50c37fa70ab64
sdlarsen1 <sdlarsen@ualberta.ca>
2024-02-01 10:01:07 -0800

    applied declarative approach for products

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ProductCategory } from '../product-categories/product-category';
-import { Product } from './product';
 import { ProductService } from './product.service';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable, catchError, of } from 'rxjs';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
 
-  products$: Observable<Product[]> | undefined;
+  products$ = this.productService.products$.pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts();
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
